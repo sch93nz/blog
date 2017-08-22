7a0e7dd2166bc935bc4a65fa1ffed40584467ec3
sch93nz <sch93nz@hotmail.com>
2017-08-22 14:18:04 +1200

    trying again to add this repository

---
+++ app/controllers/articles_controller.rb
@@ -0,0 +1,53 @@
+class ArticlesController < ApplicationController
+    
+    http_basic_authenticate_with name:"dhh", password: "secret", excep: [:index, :show]
+
+    def index
+        @articles = Article.all
+    end
+
+    def show
+        @article=Article.find(params[:id])
+    end
+
+    def new
+        @article = Article.new
+    end
+
+    def edit
+        @article =Article.find(params[:id])
+    end
+
+    def create
+        @article = Article.new(article_parms)
+
+      if @article.save
+        redirect_to @article
+      else
+        render 'new'
+      end 
+    end
+
+    def update
+        @article = Article.find(params[:id])
+
+        if @article.update(article_parms)
+            redirect_to @article
+        else
+            render 'edit'
+        end
+    end
+
+    def destroy
+        @article = Article.find(params[:id])
+        @article.destroy
+
+        redirect_to articles_path
+    end
+
+    private
+    def article_parms
+        params.require(:article).permit(:title, :text)
+    end
+
+end
